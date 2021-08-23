7c7cf2fa0ebc69bd46fea4489c4a391c51b80e47
EvanEbert <evan.ebert17@gmail.com>
2021-08-23 13:01:32 -0500

    restructured document to match ORM and MVC standards

--- server.js
+++ server.js
@@ -1,52 +1,20 @@
-
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-const notesdb = require('./db/db.json');
-const fs = require('fs');
-
+// Initialize the app and create a port
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-const PORT = 3001;
-
-
-app.use(express.urlencoded({ extended: true }));
+// Set up body parsing, static, and route middleware
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-
-app.get('/', (req, res) => {
-  res.sendFile('./public/index.html');
-});
-
-
-app.get('/notes', (req, res) => {
-  res.sendFile(__dirname + '/public/notes.html');
-})
-app.get('/api/notes', (req, res) => {
-  fs.readFile(__dirname + '/db/db.json', 'utf8', (err, data) => {
-    res.json(JSON.parse(data));
-  })
-})
-app.post('/api/notes', (req, res) => {
-  fs.readFile(__dirname + '/db/db.json', 'utf8', (err, data) => {
-      
-    var parsedData = JSON.parse(data);
-    parsedData.push(req.body);
-    console.log('parsedData', parsedData)
-    fs.writeFile(__dirname + '/db/db.json', JSON.stringify(parsedData), (err, data) => {
-      err ? console.error(err) : console.log(data);
-      res.json(parsedData);
-    })
-  })
-})
-app.delete(`/api/notes/`, (req, res) => {
-  fs.readFile('db/db.json', 'utf8', (err, data) => {
-    err ? console.error(err) : console.log(data);
-    //read the title as the id
-  })
-})
-
-app.listen(PORT, () => {
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+ten(PORT, () => {
   console.log('server on')
 });
